81dfde44750aa130ddd8b8f6a7d6c3aaf402d836
W.A. van der Meeren <danny@lm-interactive.com.cn>
2022-12-21 06:49:18 +0800

    optimize

--- challenge_20221220.cpp
+++ challenge_20221220.cpp
@@ -6,67 +6,89 @@ using namespace std;
 
 #define BUFFER_SZ 6144
 
-typedef struct 
+struct tpl_t
 {
     long long val;
     int pos;
-} tpl_t;
+    tpl_t *next;
+    tpl_t *prev;
+};
 
 tpl_t msg[BUFFER_SZ];
-tpl_t *msg_order[BUFFER_SZ];
 int msg_sz = 0;
 
-void swap (int a, int b) {
-    tpl_t tmp = msg[a];
+void putLeftOf(tpl_t *tp, tpl_t *lo) {
+    tpl_t tmp = *tp;
 
-    msg[a] = msg[b];
-    msg[b] = tmp;
+    (*lo->prev).next = tp;
+    tp->prev = lo->prev;
+    tp->next = lo;
+    lo->prev = tp;
 
-    msg_order[ msg[a].pos ] = &msg[a];
-    msg_order[ msg[b].pos ] = &msg[b];
+    (*tmp.next).prev = tmp.prev;
+    (*tmp.prev).next = tmp.next;
 }
 
-void move (int pos, long long adv) {
-    int npos;
-    int dir = adv < 0 ? -1 : 1;
+void move(tpl_t *tp, long long adv) {
+    tpl_t *lp = tp;
 
     adv = adv % (msg_sz-1);
 
-    for ( int i = 0 ; i < abs(adv) ; i++ ) {        
-
-        npos = ( pos + dir );
-        if (npos < 0) npos = msg_sz-1; 
-        if (npos >= msg_sz) npos = 0;
-
-        swap (pos, npos);
-
-        pos = npos;
+    if (adv < 0) {
+        for (int i=0; i<abs(adv);i++) 
+            lp = lp->prev;
     }
+    else if (adv > 0) {
+        for (int i=0; i<=abs(adv);i++) 
+            lp = lp->next;
+    }
+    else
+        return;
+
+    putLeftOf(tp, lp);
+}
+
+tpl_t *advance (tpl_t *start, int adv) {
+
+    for (; adv > 0; adv--)
+        start = start->next;
+
+    return start;
 }
 
 void printResult ( const string &heading )
 {
+    long long a, b, c;
+    tpl_t *tp = msg;
+
     cout << heading << endl;
 
-    long long a, b, c;
-    
-    for (int i = 0; i < msg_sz; i++) {
-        if ( msg[i].val == 0 ) {
+    do {
+        if ( tp->val == 0 ) {
 
-            a = msg[(i+1000) % msg_sz].val;
-            b = msg[(i+2000) % msg_sz].val;
-            c = msg[(i+3000) % msg_sz].val;
+            tp = advance(tp, 1000);
+            a = tp->val;
+
+            tp = advance(tp, 1000);
+            b = tp->val;
+
+            tp = advance(tp, 1000);
+            c = tp->val;
 
             printf ("1000th : %lld\n", a);
             printf ("2000th : %lld\n", b);
             printf ("3000th : %lld\n", c);
             printf ("ANSWER: %lld\n\n", a + b + c);
+
             break;
         }
-    }
+
+        tp = tp->next; 
+    } while (tp != msg);
+
 }
 
-void load (const string &fname) {
+void load (const string &fname, long long key) {
     int d;
 
     msg_sz = 0;
@@ -75,8 +97,10 @@ void load (const string &fname) {
 
     while ( ifs >> d ) {
         msg[msg_sz].pos = msg_sz;
-        msg[msg_sz].val = d;
-        msg_order[msg_sz] = &msg[msg_sz];
+        msg[msg_sz].val = ( d * key );
+
+        msg[msg_sz].next = &msg[msg_sz+1];
+        msg[msg_sz+1].prev = &msg[msg_sz];
 
         msg_sz += 1;
 
@@ -86,30 +110,25 @@ void load (const string &fname) {
             exit(-1);
         }
     }
+
+    msg[msg_sz-1].next = &msg[0];
+    msg[0].prev = &msg[msg_sz-1];
 }
 
 void solveOnce () {
-    for (int i = 0; i < msg_sz; i++) {
-        int pos = (msg_order[i] - msg);
-        move(pos, msg_order[i]->val);
-    }
+    for (int i = 0; i < msg_sz; i++)
+        move(&msg[i], msg[i].val);
 }
 
 int main() {
 
     // PART I
-    load("challenge_20221220.txt");
-
+    load("challenge_20221220.txt", 1);
     solveOnce();
-
     printResult ( "--Part I" );
 
-    
     // PART II
-    load("challenge_20221220.txt");
-
-    for (int i = 0; i < msg_sz; i++)
-        msg[i].val *= 811589153;
+    load("challenge_20221220.txt", 811589153);
 
     for (int i=0; i<10; i++)
         solveOnce();
